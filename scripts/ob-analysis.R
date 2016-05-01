bf9648660e9cb0b0ded19658b13af298808c7f20
Hannah Recht <hrecht@users.noreply.github.com>
2016-04-30 21:43:28 -0400

    start s4

--- scripts/ob-analysis.R
+++ scripts/ob-analysis.R
@@ -1,23 +1,92 @@
-#HR, 04-27-15
-#Orphan Black Analysis
-#Look at character time by episode, cloneswap times, etc
+# Orphan Black Analysis
+# Format data files for visualization
+# Look at character time by episode, cloneswap times, etc
 
+library("readxl")
 library("dplyr")
 library("tidyr")
 library("jsonlite")
 
+# Main data: 1 row per character appearance
 ob <- read.csv("data/obtimes.csv", header=T, stringsAsFactors=F)
+# Episode data - length, screen time, percent
 totaltime <- read.csv("data/totaltime.csv", header=T, stringsAsFactors=F)
+# Character metadata - origin, status, etc
 charmeta <- read.csv("data/charactermeta.csv", header=T, stringsAsFactors=F)
+# Episode titles
+titles <- read_excel("data/obs4.xlsx", sheet="titles")
 
-#Add a cloneswap variable and minutes
-ob <- ob %>% mutate(cloneswap = ifelse(character != charas, 1, 0)) %>%
-  mutate(minutes = tottime/60)
+########################################################################################################
+# Format episode-level character time data
+########################################################################################################
+
+formatEpisode <- function(dt) {
+  # Remove the separate minutes and seconds columns used for recording times, just keep # of seconds
+  dt <- dt %>% select(-startm_raw, -starts_raw, -stopm_raw, -stops_raw)
+  # Go from raw time to episode time - removing recap & credits
+  # Remove RECAP length from starttime and stoptime
+  recap <- dt$tottime[dt$character=="RECAP"]
+  dt <- dt %>% mutate(startsec = starttime_raw - recap,
+                      stopsec = stoptime_raw - recap) %>%
+    filter(character != "RECAP")
+  
+  # Remove CREDITS length from all rows after credits
+  credits <- dt$tottime[dt$character=="CREDITS"]
+  credits_row <- which(dt$character == "CREDITS")
+  
+  dt <- dt %>% mutate(startsec = ifelse(row_number(episode) < credits_row, startsec,
+                                        startsec - credits),
+                      stopsec = ifelse(row_number(episode) < credits_row, stopsec,
+                                       stopsec - credits)) %>%
+    filter(character != "CREDITS")
+  
+  # Add minutes columns and cloneswap binary variable
+  dt <- dt %>% mutate(startmin = startsec/60,
+                      stopmin = stopsec/60,
+                      minutes = (stopsec - startsec)/60) %>%
+    select(-starttime_raw, -stoptime_raw) %>% 
+    mutate(cloneswap = ifelse(character != charas, 1, 0))
+}
+
+########################################################################################################
+# Add to total time table
+########################################################################################################
+
+# Get total time and tmas time from episode, join to title
+makeEpisodeRow <- function(dt) {
+  # Usually the episode length is the maximum stoptime value
+  # Occasionally the episode ends without a Maslany character - recorded as "END"
+  epend <- max(dt$stopsec)
+  dt <- dt %>% filter(character != "END")
+  eprow <- dt %>% group_by(episode) %>%
+    summarize(tattime = sum(tottime)) %>%
+    mutate(eptime = epend, 
+           epmin = eptime/60,
+           tmasmin = tattime/60,
+           tmaspct = tattime/eptime)
+  # Add title to row
+  eprow <- left_join(eprow, titles, by="episode")
+}
+
+ep33 <- read_excel("data/obs4.xlsx", sheet="ep33")
+ep33 <- formatEpisode(ep33)
+row33 <- makeEpisodeRow(ep33)
+
+########################################################################################################
+# Add new episodes to full series dataset, total time dataset
+########################################################################################################
+
+ob <- rbind(ob, ep33)
+ob <- ob %>% arrange(episode, startsec)
 write.csv(ob, "data/obtimes.csv", row.names=F, na="")
 
-#objs <- ob %>% select(episode, startmin, stopmin, character, charas, cloneswap)
-#objson <- toJSON(objs, digits=6)
-#write(objson, "data/obtimes.json")
+totaltime <- rbind(totaltime, row33)
+totaltime <- totaltime %>% arrange(episode)
+write.csv(totaltime, "data/totaltime.csv", row.names=F, na="")
+
+########################################################################################################
+# Make character-level data files
+########################################################################################################
 
 #Collapse to get minutes by character & episode
 timebyep <- ob %>% group_by(episode, character) %>%
@@ -31,20 +100,24 @@ rm(temp)
 write.csv(timebyep, file="data/chartimebyep.csv", row.names=F, na="")
 
 #Data for table: total time by character, # of episodes, cloneswaps
-chartable <- timebyep %>% mutate(p = 1) %>%
-  group_by(character) %>%
-  summarize(episodesin = sum(p),
+chartable <- timebyep %>% group_by(character) %>%
+  summarize(episodesin = n(),
           minutes = sum(minutes)) %>%
   arrange(desc(minutes)) 
+
 chartable <- left_join(charmeta, chartable, by="character")
+
 write.csv(chartable, file="data/charactertable.csv", row.names=FALSE)
+
+########################################################################################################
+# Miscellaneous data
+########################################################################################################
 
 #Get total screen time by clone & cloneswap
 cloneswap <- ob %>% group_by(character, charas) %>%
   summarize(minutes = sum(minutes)) %>%
   filter(character != charas)
 
+# Headline numbers for clone tracker
 sum(totaltime$epmin)
 sum(totaltime$tmasmin)
-
-
